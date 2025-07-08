439413a03ae88981ea18442490d4e60d9bef5335
maskrenhas2022 <maskrenhas02@.gmail.com>
2025-07-07 21:19:14 -0400

    Start new  Project RockeatSeat

---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { fastify } from "fastify"
+import{
+
+    serializerCompiler,
+    validatorCompiler,
+  type ZodTypeProvider,
+
+
+} from 'fastify-type-provider-zod'
+
+
+import {fastifyCors} from '@fastify/cors'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+
+origin: 'http://localhost:5173',
+
+})
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.get('/health', () =>{
+    return 'MASKRENHAS OK'
+})
+
+
+app.listen({ port: process.env.PORT ? Number(process.env.PORT) : 3333}).then( () =>{
+    
+    console.log(`Port: ${process.env.Port}`)
+    
+    console.log('MaskrenhasDev Server running!')
+})
